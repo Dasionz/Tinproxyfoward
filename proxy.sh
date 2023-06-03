7bf4628c4b96b22d5574c75baea2da3dcf1114e0
dasionz123 <55187922+dasionz123@users.noreply.github.com>
2023-06-04 01:28:59 +0700

    proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-# API configuration
-API_KEY=""
+# Function to get a new proxy from tinproxy API
+get_new_proxy() {
+  local api_key="YOUR_API_KEY"
+  local authen_ips="YOUR_AUTHEN_IPS"
+  local location="YOUR_LOCATION"
 
-# Local server configuration
-LOCAL_SERVER_PORT="6789"
+  local response=$(curl -s "https://api.tinproxy.com/proxy/get-new-proxy?api_key=$api_key&authen_ips=$authen_ips&location=$location")
+  local code=$(echo "$response" | jq -r '.code')
+  local message=$(echo "$response" | jq -r '.message')
 
-# Get API key from user input
-echo "Enter your API key:"
-read -r API_KEY
+  if [ "$code" -eq 1 ]; then
+    echo "Failed to get new proxy: $message"
+    exit 1
+  fi
 
-# Proxy forwarding
+  local http_ipv4=$(echo "$response" | jq -r '.data.http_ipv4')
+  local username=$(echo "$response" | jq -r '.data.authentication.username')
+  local password=$(echo "$response" | jq -r '.data.authentication.password')
+
+  echo "$http_ipv4:$username:$password"
+}
+
+# Function to forward the proxy to your local server
+forward_proxy() {
+  local proxy="$1"
+  local local_server_port="6789"
+
+  socat TCP-LISTEN:$local_server_port,fork,su=nobody TCP:$proxy &
+  echo "Proxy forwarding enabled. Proxy: $proxy -> Local server: localhost:$local_server_port"
+}
+
+# Main script logic
 while true; do
-   # Get new proxy
-   response=$(curl -s "https://api.tinproxy.com/proxy/get-new-proxy?api_key=$API_KEY")
-   proxy_http=$(echo "$response" | jq -r '.data.http_ipv4')
-   proxy_username=$(echo "$response" | jq -r '.data.authentication.username')
-   proxy_password=$(echo "$response" | jq -r '.data.authentication.password')
-
-   # Forward proxy to local server
-   socat TCP-LISTEN:$LOCAL_SERVER_PORT,fork PROXY:$proxy_http,proxyport=$LOCAL_SERVER_PORT,proxyauth=$proxy_username:$proxy_password &
-
-   # Wait for 200 seconds
-   sleep 200
-
-   # Terminate the socat process
-   pkill socat
+  proxy=$(get_new_proxy)
+  forward_proxy "$proxy"
+  sleep 200
 done
