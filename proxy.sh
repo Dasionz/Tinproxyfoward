790c77342567afd52dca97aad66ed0bba26e5f9f
dasionz123 <55187922+dasionz123@users.noreply.github.com>
2023-06-04 04:04:04 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+# Read the API key
 read -p "Enter your API Key: " api_key
 
-# Function to get a new proxy from tinproxy API
-get_new_proxy() {
-  local response=$(curl -s "https://api.tinproxy.com/proxy/get-new-proxy?api_key=$api_key")
+# The location of the Squid configuration file
+squid_config="/etc/squid/squid.conf"
 
-  local http_ipv4=$(echo "$response" | jq -r '.data.http_ipv4')
-  local username=$(echo "$response" | jq -r '.data.authentication.username')
-  local password=$(echo "$response" | jq -r '.data.authentication.password')
-
-  echo "$http_ipv4:$username:$password"
-}
-
-# Main script logic
 while true; do
-  proxy=$(get_new_proxy)
-  
-  # Update Tinyproxy configuration
-  sed -i "/upstream http/c\upstream http \"http://$proxy\"" /etc/tinyproxy/tinyproxy.conf
-  
-  # Restart Tinyproxy
-  sudo service tinyproxy restart
-  
-  sleep 200
+    # Get a new proxy from tinproxy
+    response=$(curl -s "https://api.tinproxy.com/proxy/get-new-proxy?api_key=$api_key")
+    http_ipv4=$(echo "$response" | jq -r '.data.http_ipv4')
+    username=$(echo "$response" | jq -r '.data.authentication.username')
+    password=$(echo "$response" | jq -r '.data.authentication.password')
+
+    # Extract the proxy address and port
+    proxy_address=$(echo $http_ipv4 | cut -d ':' -f 1)
+    proxy_port=$(echo $http_ipv4 | cut -d ':' -f 2)
+
+    # Update the Squid configuration file with the new proxy details
+    sed -i "s/^cache_peer .*/cache_peer $proxy_address parent $proxy_port 0 no-query default login=$username:$password/" $squid_config
+
+    # Restart Squid to apply the new proxy
+    systemctl restart squid
+
+    # Wait 200 seconds before getting a new proxy
+    sleep 200
 done
